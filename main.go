dbdb51c80c91fb24a09910679d974504ca1c7d13
David Alberto Gomez Rios <dgomezr@proteccion.com.co>
2024-03-21 16:24:26 -0500

    Commit siguiente

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang/variables"
+)
 
 func main() {
 	fmt.Println("Hola mundo")
+	variables.MostrarEnteros()
+	variables.RestoVariables()
+	estado, texto := variables.ConviertoaTexto(1549)
+	fmt.Println(estado)
+	fmt.Println(texto)
+
 }
